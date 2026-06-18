f3611cb1adb4d98a390b8fe79b4d3779eac8b99c
shafeeq-28 <shafeeq28.2005@gmail.com>
2026-06-18 20:57:45 +0530

    Fix complaints table schema

--- db.py
+++ db.py
@@ -6,23 +6,28 @@ def create_table():
 
     cursor = conn.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS complaints(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT,
-        name TEXT,
-        incident TEXT,
-        location TEXT,
-        date TEXT,
-        crime_type TEXT,
-        status TEXT,
-        formal_complaint TEXT
+    cursor.execute(
+        "DROP TABLE IF EXISTS complaints"
     )
-    """)
+
+    cursor.execute(
+        """
+        CREATE TABLE complaints(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            name TEXT,
+            email TEXT,
+            incident TEXT,
+            location TEXT,
+            date TEXT,
+            crime_type TEXT,
+            status TEXT,
+            formal_complaint TEXT
+        )
+        """
+    )
 
     conn.commit()
-
     conn.close()
-
 
 create_table()
